d31f4c31deeee3cc10d949127a8929365ccc14d3
alex <sashason1990@gmail.com>
2024-06-12 03:42:13 +0300

    ss

--- register.py
+++ register.py
@@ -2,6 +2,9 @@ from pprint import pprint
 import fake_useragent
 import httpx
 import json
+import urllib.parse
+import time
+from random_username.generate import generate_username
 
 
 
@@ -35,17 +38,17 @@ CHECK = "https://gateway.blum.codes/v1/user/username/check"
 
 s = "104.239.86.231!6141!pbomsduk!t3flfdg7jrxi|216.173.109.238!6469!pbomsduk!t3flfdg7jrxi|216.173.109.80!6311!pbomsduk!t3flfdg7jrxi|138.128.145.203!6122!pbomsduk!t3flfdg7jrxi|64.137.66.249!5834!pbomsduk!t3flfdg7jrxi|64.137.103.238!6826!pbomsduk!t3flfdg7jrxi|64.137.66.83!5668!pbomsduk!t3flfdg7jrxi|64.137.88.35!6274!pbomsduk!t3flfdg7jrxi|216.173.103.251!6765!pbomsduk!t3flfdg7jrxi|64.137.108.167!5760!pbomsduk!t3flfdg7jrxi|104.239.86.22!5932!pbomsduk!t3flfdg7jrxi|64.137.108.17!5610!pbomsduk!t3flfdg7jrxi|209.99.129.89!6077!pbomsduk!t3flfdg7jrxi|"
 
-accs = []
+# accs = []
 
-with open("accounts.txt", "r") as f:
-    for i in f.readlines():
-        n = i.split()
-        accs.append(
-            {
-                "number": n[0],
-                "proxy": n[1],
-            }
-        )
+# with open("accounts.txt", "r") as f:
+#     for i in f.readlines():
+#         n = i.split()
+#         accs.append(
+#             {
+#                 "number": n[0],
+#                 "proxy": n[1],
+#             }
+#         )
         
 
 
@@ -84,6 +87,8 @@ class Profile:
     client: httpx.Client
     user_agent: str
     query: str
+    username: str
+    referral_token: str
     headers = {
             "content-type": "application/json",
             "accept": "application/json, text/plain, */*",
@@ -95,36 +100,61 @@ class Profile:
             "sec-fetch-dest": "empty"
         }
     
-    def __init__(self, phone: str, proxy_data: dict, query: str, user_agent: str = "") -> None:
+    def __init__(self, phone: str, proxy_data: dict, query: str, username: str, user_agent: str = "") -> None:
         self.proxy = Proxy(proxy_data)
         self.phone = phone
         self.client = httpx.Client(proxy=str(self.proxy))
         self.user_agent = user_agent
-        self.query = query
         self.headers["user-agent"] = self.user_agent
-            
+        self.username = username
+        self.query = query
+        self.make_query()
+        self.referral_token = urllib.parse.parse_qs(query)["start_param"][0][4:]
+    
+    def make_query(self):
+        current_timestamp = int(time.time())
+        encoded_str = self.query
+        params = urllib.parse.parse_qs(encoded_str)
+        print(params)
+        user_json = urllib.parse.unquote(params['user'][0])
+        user_data = json.loads(user_json)
+        user_data['username'] = self.username
         
-
+        auth_date_json = urllib.parse.unquote(params['auth_date'][0])
+        auth_date_data = json.loads(auth_date_json)
+        auth_date_data = str(current_timestamp)
+        
+        
+        updated_user_json = json.dumps(user_data)
+        updated_user_encoded = urllib.parse.quote(updated_user_json)
+        
+        updated_auth_data_json = json.dumps(auth_date_data)
+        updated_auth_data_encoded = urllib.parse.quote(updated_auth_data_json)
+        
+        params['user'][0] = updated_user_encoded
+        params['auth_date'][0] = updated_auth_data_encoded
+        updated_encoded_str = urllib.parse.urlencode(params, doseq=True)
+        return updated_encoded_str
 
 
 class ProfileService:
     def new_profile(self, number: str, proxy: dict):
         with open("data.json", "r") as f:
-            accs = json.loads(f)
+            accs = json.load(f)
         accs["accs"][number] = {
             "proxy": self.get_free_proxy(),
             "user-agent": fake_useragent.UserAgent(platforms="mobile").random
         }
     
-    def use_profile(id: int) -> Profile:
+    def use_profile(self, id: int) -> Profile:
         with open("data.json", "r") as f:
-            all_accs = json.loads(f)
-        acc = all_accs[id]
+            all_accs = json.load(f)
+        acc = all_accs[str(id)]
         if not acc["user-agent"]:
             acc["user-agent"] = fake_useragent.UserAgent(platforms="mobile").random
         with open("data.json", "w") as f:
-            json.dumps(all_accs, f)
-        profile = Profile(acc["phone"], acc["proxy"], acc["user-agent"])
+            json.dump(all_accs, f)
+        profile = Profile(phone=acc["phone"], proxy_data=acc["proxy"], user_agent=acc["user-agent"], query=acc["query"], username=acc["username"])
         return profile
 
 
@@ -134,14 +164,37 @@ class RegisterService:
         self.profile_service = profile_service
 
     def create_one(self, id: int):
-        requester = self.profile_service.use_profile(id)
-        resp = requester.client.post(AUTH_URL)
-        data = requester.post("register")
-        return 
+        requester = self.profile_service.use_profile(id=id)
+        
+        if not self.check_username(requester, requester.username):
+            new_username = generate_username()[0]
+            while not self.check_username(requester, new_username):
+                new_username = generate_username()[0]
+        
+        requester.username = new_username
+        
+        content = {
+            "query": requester.query,
+            "username": requester.username,
+            "referralToken": requester.referral_token
+        }
+        
+        resp = requester.client.post(AUTH_URL, headers=requester.headers, json=json.dumps(content))
+        return resp.status
+    
+    def check_username(self, profile: Profile, username: str):
+        resp = profile.client.post(url=CHECK, json=json.dumps({"username": username}), headers=profile.headers)
+        print(username, resp)
+        return True if resp.status_code == 200 else False     
+        
 
 
     def create_many(self, ids: list):
         pass
+
+
+test = RegisterService(ProfileService())
+print(test.create_one(id=1))
 
 
 # register(query='sфыв', username='s', referral_token='asdasd', avatar_file_key='asdasd')    
---
+++ test.py
@@ -0,0 +1,51 @@
+import urllib.parse
+import json
+import time
+# Исходная строка
+encoded_str = "user=%7B%22id%22%3A7493728124%2C%22first_name%22%3A%22darvin%22%2C%22last_name%22%3A%22tink%22%2C%22language_code%22%3A%22ru%22%2C%22allows_write_to_pm%22%3Atrue%7D&chat_instance=3051893017577474126&chat_type=private&start_param=ref_vbaxovVKQr&auth_date=1718050772&hash=54935857deb179557aabc58805fd2df0f0c6210af8084ff0bf87c08b67b90068"
+
+# Парсим строку на отдельные параметры
+params = urllib.parse.parse_qs(encoded_str)
+print(params)
+
+# Декодируем значение параметра 'user'
+user_json = urllib.parse.unquote(params['user'][0])
+
+ref = params["start_param"][0][4:]
+print(ref)
+
+# Преобразуем JSON-строку в словарь Python
+user_data = json.loads(user_json)
+
+current_timestamp = int(time.time())
+# Меняем значение 'username'
+user_data['username'] = 'new_username'
+
+# Преобразуем обновленный словарь обратно в JSON-строку
+updated_user_json = json.dumps(user_data)
+
+# Кодируем JSON-строку
+updated_user_encoded = urllib.parse.quote(updated_user_json)
+
+# Обновляем параметр 'user' в словаре параметров
+params['user'][0] = updated_user_encoded
+auth_date_json = urllib.parse.unquote(params['user'][0])
+auth_date_data = json.loads(auth_date_json)
+auth_date_data['auth_date'] = str(current_timestamp)
+updated_auth_data_json = json.dumps(auth_date_data)
+updated_auth_data_encoded = urllib.parse.quote(updated_auth_data_json)
+params['auth_date'][0] = updated_auth_data_encoded
+
+# Собираем строку с обновленными параметрами
+updated_encoded_str = urllib.parse.urlencode(params, doseq=True)
+
+# Выводим обновленную строку
+print(updated_encoded_str)
+
+"query_id=AAGtmaIWAwAAAK2ZohZ7Lkp6&user=%7B%22id%22%3A6822205869%2C%22first_name%22%3A%22aleksandr%22%2C%22last_name%22%3A%22%22%2C%22language_code%22%3A%22ru%22%2C%22allows_write_to_pm%22%3Atrue%7D&auth_date=1718150955&hash=e36d1ae00feccd3e8382029c461d4b35aeeda93837a85df390abd425864a1ad8"
+
+
+with open("data.json", "r") as f:
+        all_accs = json.load(f)
+
+print(all_accs)
